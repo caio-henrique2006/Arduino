42c1678d67575e40b66a220e1e919d1c7c504bb0
caio-henrique2006 <caio_henrique2006@outlook.com>
2022-07-14 20:34:32 -0300

    .

---
+++ AllLinesUsed.ino/AllLinesUsed.ino
@@ -0,0 +1,20 @@
+/*
+
+void setup(){
+}
+void loop() {
+}
+
+pinMode()
+digitalWrite(pin, state);
+analogWrite(pin, state);
+digitalRead(pin);
+analogRead(pin);
+tone(value);
+noTone();
+Serial
+Serial.begin(9600);
+Serial.println(return);
+Serial.print(return);
+
+*/
